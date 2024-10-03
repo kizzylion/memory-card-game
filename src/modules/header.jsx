f94e47a94258353ace828d3b8bb048ad0b11c7ac
kizzylion <kztchm@gmail.com>
2024-10-03 13:49:46 -0400

    ADD FAVICON

--- src/modules/header.jsx
+++ src/modules/header.jsx
@@ -1,9 +1,11 @@
+import logo from "../assets/logo.png";
+
 const Header = () => {
   return (
     <header>
       <div className="max-w-7xl px-4 pt-4 pb-4 md:px-7 lg:px-20">
         <div>
-          <img src="src/assets/logo.png" alt="" className="h-11 md:h-14 " />
+          <img src={logo} alt="" className="h-11 md:h-14 " />
         </div>
       </div>
     </header>
