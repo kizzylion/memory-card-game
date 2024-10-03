04c1dce762ca700c90b78ae54b1ecb6e36d3c383
kizzylion <kztchm@gmail.com>
2024-10-03 16:50:14 -0400

    ADD COMPLETE STYLE TO GAME CARD

--- src/modules/pokemon-card.jsx
+++ src/modules/pokemon-card.jsx
@@ -1,8 +1,8 @@
 const PokemonCard = ({ pokemon }) => {
   return (
-    <div className="card flex flex-col items-center justify-center  w-full h-fit">
+    <div className="card flex flex-col items-center justify-center  w-full h-fit bg-yellow-50/60 overflow-clip border-4  rounded-b-xl">
       <div className="aspect-video w-full h-full flex relative  ">
-        <div className=" absolute card-bg w-full h-full -z-1 bg-zinc-500">
+        <div className=" absolute card-bg w-full h-full -z-1 bg-zinc-500  ">
           <img
             src="src/assets/card-background.png"
             className=" z-10   mx-auto object-cover  mix-blend-multiply"
@@ -13,7 +13,9 @@ const PokemonCard = ({ pokemon }) => {
           className=" z-10  h-4/5 mx-auto my-auto object-cover"
         />
       </div>
-      <p className="text-xl font-bold">{pokemon.name}</p>
+      <p className="pokemon-solid  text-lg  p-1 border-t-4 bg-yellow-300/60 border-zinc-50/50 w-full text-center capitalize text-blue-800 ">
+        {pokemon.name}
+      </p>
     </div>
   );
 };
